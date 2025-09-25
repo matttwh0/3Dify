b1c9e9d5748eba69c0ec19ab08936ea181ba1dd3
matttwh0 <matttran2004@gmail.com>
2025-09-24 18:28:10 -0700

    hello world component

--- 3Dify/src/App.jsx
+++ 3Dify/src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Message from './Message';
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <div><Message/></div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
