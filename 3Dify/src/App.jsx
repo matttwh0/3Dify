fcaa8862e2a3fa1ecd073650013037330e6268bd
matttwh0 <matttran2004@gmail.com>
2025-09-29 13:34:57 -0700

    Add dashboard template

--- 3Dify/src/App.jsx
+++ 3Dify/src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
 import './App.css'
 import Message from './Message';
+import LandingPage from './LandingPage';
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <div><Message/></div>
+    <div>
+      {/*<Message/>*/}
+      </div>
       <div>
-        <h1 class="text-3x1 font-bold underline">Hello World!</h1>
+        <LandingPage/>
       </div>
     </>
   )
