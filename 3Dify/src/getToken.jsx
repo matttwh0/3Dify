f6404d8aa4e3f8ad2703e96c016027199ebb27a2
matttwh0 <matttran2004@gmail.com>
2025-10-01 22:51:10 -0700

    create token POST request + button
    in GetToken.jsx

--- 3Dify/src/getToken.jsx
+++ 3Dify/src/getToken.jsx
@@ -3,25 +3,52 @@ This gets the token ID to make a photoscene POST request
 npm install js-based64
 */
 
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-class getToken extends Component {
-//TODO: use btoa()
-autodeskURI = 'https://developer.api.autodesk.com/photo-to-3d/v1/photoscene'
-originalString = "GAjgqsQ2Xk0HmDGljBblQ5oio0jEY5AXI87YNzJJG0iYQnxX:jpK99MVLblBZCQ1NSx4exKi49sy5mNZP8D8M30xc4AwflzAUWwnUP8RmTkdYlrot"
-encodedString = window.btoa(originalString);
+class GetToken extends Component {
+  //TODO: use btoa()
+  GetTokenURI = "https://developer.api.autodesk.com/authentication/v2/token";
 
-claims = {
-    first_name: '',
-    last_name: '',
-    user_email: '',
-    
-}
+  constructor(props) {
+    super(props);
+    this.originalString =
+      "GAjgqsQ2Xk0HmDGljBblQ5oio0jEY5AXI87YNzJJG0iYQnxX:jpK99MVLblBZCQ1NSx4exKi49sy5mNZP8D8M30xc4AwflzAUWwnUP8RmTkdYlrot";
+    this.encodedString = window.btoa(this.originalString);
+
+    this.state = {
+      first_name: "Matthew",
+      last_name: "Tran",
+      user_email: "matttran2004@gmail.com",
+    };
+  }
+
+  headers = {
+    "Content-Type": "application/x-www-form-urlencoded",
+    Authorization: "Basic" + this.encodedString,
+  };
+
+   clickHandler = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+
+    axios.post(this.GetTokenURI)
+  }
   render() {
+    const { scenename } = this.state;
     return (
-      <div>getToken</div>
-    )
+      <div>
+        <form>
+          <div>
+            <button 
+            onClick = {this.clickHandler}
+            className="bg-gray-400 text-black p-2 m-4 w-30 h-20"
+            type = "submit">Generate Token
+            </button>
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
-export default getToken
+export default GetToken;
