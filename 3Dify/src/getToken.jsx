35e30b6653f7f78e21334aa83b001bec51211dc1
matttwh0 <matttran2004@gmail.com>
2025-10-02 00:27:35 -0700

    token auth post request complete
    can now view token auth

--- 3Dify/src/getToken.jsx
+++ 3Dify/src/getToken.jsx
@@ -4,6 +4,7 @@ npm install js-based64
 */
 
 import React, { Component } from "react";
+import axios from "axios";
 
 class GetToken extends Component {
   //TODO: use btoa()
@@ -15,34 +16,44 @@ class GetToken extends Component {
       "GAjgqsQ2Xk0HmDGljBblQ5oio0jEY5AXI87YNzJJG0iYQnxX:jpK99MVLblBZCQ1NSx4exKi49sy5mNZP8D8M30xc4AwflzAUWwnUP8RmTkdYlrot";
     this.encodedString = window.btoa(this.originalString);
 
-    this.state = {
-      first_name: "Matthew",
-      last_name: "Tran",
-      user_email: "matttran2004@gmail.com",
+    this.headers = {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Accept: "application/json",
+      Authorization: `Basic ${this.encodedString}`,
     };
   }
 
-  headers = {
-    "Content-Type": "application/x-www-form-urlencoded",
-    Authorization: "Basic" + this.encodedString,
-  };
+  body = new URLSearchParams({
+    grant_type: "client_credentials",
+    scope: "data:read",
+  });
 
-   clickHandler = (e) => {
+  clickHandler = (e) => {
     e.preventDefault();
-    console.log(this.state);
+    console.log(this.body);
 
-    axios.post(this.GetTokenURI)
-  }
+    axios
+      .post(this.GetTokenURI, this.body, { headers: this.headers })
+      .then((response) => {
+        const token = response.data.access_token; //our token 
+        console.log(`Token: ${token}`);
+        this.setState({token}); //store token in state 'this.state.token'
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   render() {
-    const { scenename } = this.state;
     return (
       <div>
         <form>
           <div>
-            <button 
-            onClick = {this.clickHandler}
-            className="bg-gray-400 text-black p-2 m-4 w-30 h-20"
-            type = "submit">Generate Token
+            <button
+              onClick={this.clickHandler}
+              className="bg-gray-400 text-black p-2 m-4 w-30 h-20"
+              type="submit"
+            >
+              Generate Token
             </button>
           </div>
         </form>
