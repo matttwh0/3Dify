7b6497aa940757df209cda7640c6bc2f16deae5d
matttwh0 <matttran2004@gmail.com>
2025-10-20 00:09:15 -0700

    new get token component
    converted the old get token 'class' component into a 'function' component for more modularity and passing of state between components

---
+++ 3Dify/src/TestGetToken.jsx
@@ -0,0 +1,66 @@
+import React, { Component, useState } from "react";
+
+import axios from "axios";
+
+export function TestGetToken(){
+
+    const [token, setToken] = useState(null);
+    const [error, setError] = useState(null);
+
+    const getTokenURI = "https://developer.api.autodesk.com/authentication/v2/token";
+    const originalString = "GAjgqsQ2Xk0HmDGljBblQ5oio0jEY5AXI87YNzJJG0iYQnxX:jpK99MVLblBZCQ1NSx4exKi49sy5mNZP8D8M30xc4AwflzAUWwnUP8RmTkdYlrot";
+    const encodedString = window.btoa(originalString);
+    
+    //headers are 
+    const headers = {
+            "Content-Type": "application/x-www-form-urlencoded",
+            Accept: "application/json",
+            Authorization: `Basic ${encodedString}`,
+        };
+    
+    //body parameters must be url encoded 
+    const body = new URLSearchParams({
+        grant_type: "client_credentials",
+        scope: "data:read",
+    });
+
+    //handles post request when button is clicked
+    async function handleClick(e){
+        e.preventDefault(); //stop form submit reload
+
+        try{
+            //sends post request and adds it to state
+            const res = await axios.post(getTokenURI, body, {headers: headers});
+            setToken(res.data.access_token);
+            console.log(`full response: ${res.data.access_token}`);
+        }
+        catch (err){
+            setError(err?.response?.data || err.message);
+            console.error(err);
+        }
+        }
+    
+    return(
+        <div>
+            <form>
+                <div>
+                    <button
+                    onClick = {handleClick}
+                    className="bg-gray-400 text-black p-2 m-4 w-30 h-20"
+                    type = "button"
+                    >
+                    Generate Token Test
+                    </button>
+                </div>
+            </form>
+        </div>
+    );
+}
+
+
+
+export function TestPostForm(){
+    return(
+        console.log('test post form working')
+    );
+}
