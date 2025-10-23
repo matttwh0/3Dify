2d93c18c067aa71ce9d13ef1e13ee7d29a939c4a
svrdgo <svrdgo@gmail.com>
2025-10-22 18:01:06 -0700

    Set up user auth and user creation

--- 3Dify/src/App.jsx
+++ 3Dify/src/App.jsx
@@ -1,20 +1,20 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
 import Message from './Message';
 import LandingPage from './LandingPage';
-function App() {
-  const [count, setCount] = useState(0)
-
+import SignInPage from './SignInPage'
+import SignUpPage from './SignUpPage'; 
+export default function App() {
   return (
-    <>
-    <div>
-      {/*<Message/>*/}
-      </div>
-      <div>
-        <LandingPage/>
-      </div>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<SignInPage />} />
+        <Route path="/LandingPage" element={<LandingPage />} />
+        <Route path="/SignUpPage" element={<SignUpPage />} />
+
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
