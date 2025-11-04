f45523650e4823eaf7e51b114297dc5312e8c48b
svrdgo <svrdgo@gmail.com>
2025-11-03 18:03:23 -0800

    file hierarchy changes and fixed placeholders
    Moved pages to their own folder and changed placeholder values to show user info

--- 3Dify/src/pages/SignInPage.jsx
+++ 3Dify/src/pages/SignInPage.jsx
@@ -9,45 +9,40 @@
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
-import { getAuth, sendEmailVerification, signInWithEmailAndPassword } from "firebase/auth";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { connectAuthEmulator } from "firebase/auth";
-import { firebaseApp } from ".";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { firebaseApp } from "..";
 const auth = getAuth(firebaseApp); // get Firebase Auth instance
 connectAuthEmulator(auth, "http://localhost:9099");
 
 
-export default function SignUpPage() {
+export default function SignInPage() {
     const [error, setError] = useState("");
     const navigate = useNavigate();
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
-    const handleSignUp = async (e) => {
+    const handleLogin = async (e) => {
     e.preventDefault();
     
-   try {
-    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-    const user = userCredential.user;
-
-    await sendEmailVerification(user);
-    console.log("Verification email sent to:", user.email);
-    alert("Check your inbox for a verification email!");
-  } catch (error) {
-    console.error("Error signing up:", error.message);
-    setError("Unable to create user")
-  }
+    try {
+      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      console.log("Signed in:", userCredential.user);
+      navigate("/LandingPage")
+    } catch (error) {
+      console.error("Error signing in:", error.message);
+      setError("Invalid email or password. Please try again.");
+    }
   };
-
   return (
 <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
   <div className="sm:mx-auto sm:w-full sm:max-w-sm">
     <img src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" className="mx-auto h-10 w-auto" />
-    <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-white">Create an account</h2>
+    <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-white">Sign in to your account</h2>
   </div>
 
   <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-    <form onSubmit={handleSignUp} action="#" method="POST" className="space-y-6">
+    <form onSubmit={handleLogin} action="#" method="POST" className="space-y-6">
   <div>
     <label htmlFor="email" className="block text-sm/6 font-medium text-gray-100">
       Email address
@@ -68,7 +63,7 @@ export default function SignUpPage() {
 
   <div>
     <label htmlFor="password" className="block text-sm/6 font-medium text-gray-100">
-      Create a password
+      Password
     </label>
     <div className="mt-2">
       <input
@@ -92,11 +87,19 @@ export default function SignUpPage() {
     type="submit"
     className="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
   >
-    Sign up
+    Sign in
   </button>
 </form>
 
-
+<p className="mt-10 text-center text-sm/6 text-gray-400">
+  Not a member? 
+  <Link
+    to="/signuppage"
+    className="font-semibold text-indigo-400 hover:text-indigo-300"
+  >
+    Create an account
+  </Link>
+</p>
 
   </div>
 </div>
