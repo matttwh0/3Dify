00208943d9149bd70b1ae9d32fc78b71244d858a
svrdgo <svrdgo@gmail.com>
2025-11-17 18:22:49 -0800

    Deleted line that is no longer needed

--- 3Dify/src/App.jsx
+++ 3Dify/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
-import Message from './Message';
 import LandingPage from './pages/LandingPage';
 import SignInPage from './pages/SignInPage'
 import SignUpPage from './pages/SignUpPage'; 
