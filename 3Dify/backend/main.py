728b549d5cd01577004753b971e8439dcd40c528
matttwh0 <matttran2004@gmail.com>
2025-12-01 20:35:02 -0800

    kiri api working

--- 3Dify/backend/main.py
+++ 3Dify/backend/main.py
@@ -1,7 +1,7 @@
 from flask import Flask, Blueprint, jsonify
 from flask_cors import CORS
-from blueprints.video_submit import video_submit_bp 
-from blueprints.post_photoscene import post_photoscene_bp
+#from blueprints.video_submit import video_submit_bp 
+#from blueprints.post_photoscene import post_photoscene_bp
 
 app = Flask(__name__)
 CORS(app, resources={
@@ -14,8 +14,8 @@ CORS(app, resources={
 })
 
 #register blueprints 
-app.register_blueprint(video_submit_bp)
-app.register_blueprint(post_photoscene_bp)
+#app.register_blueprint(video_submit_bp)
+#app.register_blueprint(post_photoscene_bp)
 
 @app.route("/")
 def helloWorld():
